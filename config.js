70ccc83753c3ae0d32a3f03213961cd4b4e8b38b
Morteza Aliyari <aliyari@outlook.com>
2020-06-18 00:08:33 +0430

    change secret key

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
   PORT: 5000,
   MONGODB_URI:
     "mongodb+srv://morteza:13675454@cluster0-oozja.mongodb.net/pm_db?retryWrites=true&w=majority",
-  SECRET_KEY: "This is secret key"
+  SECRET_KEY:
+    "kKjv-2uwr0-gMUK3u3Rvi4C0yczHgftqTHMOYdfYWqo8MNC-28dVlDtkCPJtGrCIRLY_Np8wmufiIR-3xznVqD7e9pbDhhA_xV6gySiV4VXA1Q6sVCKPtYNIbOzg38h7KOEtA_W3vPCikQrJZ4z536d6d20dCL0xXjvcGZXqRK14pqLRtqKVwlrNcMD3NneH5zk3lKxpJZ0LhBgYVDj8ANrqS6RovAHGsvkXeunlAoe6ZoOlKaaJ8iwbm6JZxhjgqqbs4n3cJpyGq9yiYDkekVcNpBiCpHnhbe1BviZF6AToFKUAXBTdLFuvaYzQlwWYYq8hJjD6pfskwg5-qgS2Dw"
 };
