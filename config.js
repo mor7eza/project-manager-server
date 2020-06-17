9c6f7d8c7925f4a43fef18284cd9e8a5b34d2298
Morteza Aliyari <aliyari@outlook.com>
2020-06-17 23:43:25 +0430

    all

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: 5000,
   MONGODB_URI:
-    "mongodb+srv://morteza:13675454@cluster0-oozja.mongodb.net/pm_db?retryWrites=true&w=majority"
+    "mongodb+srv://morteza:13675454@cluster0-oozja.mongodb.net/pm_db?retryWrites=true&w=majority",
+  SECRET_KEY: "This is secret key"
 };
